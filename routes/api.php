714f15fa146653b25bfc4e6550b03dc82310e8a1
Ammar Fadhlur Rahman <afrastgeek@gmail.com>
2018-02-10 01:27:33 +0700

    Product List API

--- routes/api.php
+++ routes/api.php
@@ -13,8 +13,19 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 Route::post('/login', 'Api\Auth\LoginController@login');
+
+Route::middleware('auth:api')->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+
+    Route::prefix('product')->group(function () {
+        Route::get('/', function () {
+            return App\Product::all();
+        });
+        Route::get('/{id}', function ($id) {
+            return App\Product::findOrFail($id);
+        });
+    });
+});
