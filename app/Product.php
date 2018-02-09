81e004635312ccb818f9e527ca4bbb8eaaaa15b6
Ammar Fadhlur Rahman <afrastgeek@gmail.com>
2018-02-09 23:55:29 +0700

    Add Category Model
    and it's relationship with Product

--- app/Product.php
+++ app/Product.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    /**
+     * Get the category that owns the product.
+     */
+    public function category()
+    {
+        return $this->belongsTo('App\Category');
+    }
 }
